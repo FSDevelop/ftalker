6e2599629ac6a1b492de6dfed90ff907ac864ca4
Federico Sosa <federico.sosa@modelit.xyz>
2016-09-25 18:00:02 -0300

    Implemented Angular.js

---
+++ public/js/main.controller.js
@@ -0,0 +1,13 @@
+/**
+ * Main Angular controller
+ * @author  Federico Sosa (federico.sosa@modelit.xyz)
+ * @since   September 2016
+ */
+ 
+var app = angular.module('app', []);
+
+app.controller('mainController', ['$scope', function($scope) {
+    $scope.init = function() {
+        
+    }
+}]);
