68b12eeee868e28b44fdae3f4b9fbb16e6b53ebe
Federico Sosa <federico.sosa@modelit.xyz>
2016-09-25 18:19:52 -0300

    Implemented mongodb

--- app.js
+++ app.js
@@ -6,6 +6,20 @@
 var express = require('express');
 var app = express();
 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/ftalker';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db.close();
+});
+
 app.use(express.static('public'));
 
 app.listen(8080, function() {
--- public/js/main.controller.js
+++ public/js/main.controller.js
@@ -9,5 +9,5 @@ var app = angular.module('app', []);
 app.controller('mainController', ['$scope', function($scope) {
     $scope.init = function() {
         
-    }
+    };
 }]);
